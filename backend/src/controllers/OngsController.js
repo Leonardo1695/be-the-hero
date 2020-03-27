8b2a69ed283c7520eb9737abede862cc263d2ef8
Leonardo1695 <leonardo.serra16@yahoo.com>
2020-03-27 19:04:17 -0300

    frontend v1

--- backend/src/controllers/OngsController.js
+++ backend/src/controllers/OngsController.js
@@ -4,19 +4,24 @@ const crypto = require('crypto')
 module.exports = {
     async index (req, res) {
         const ongs = await connection('ongs').select('*')
+        //await connection('ongs').select('*').delete()
     
         return res.json(ongs)
+
+        // return res.json({})
     },
 
     async create (req, res) {
         const { ...data } = req.body
+
+        try {
+            data.id = crypto.randomBytes(4).toString('HEX')
     
-        console.log(data)
-    
-        data.id = crypto.randomBytes(4).toString('HEX')
-    
-        await connection('ongs').insert(data)
-    
-        return res.json({ status: 'success!' })
+            await connection('ongs').insert(data)
+        
+            return res.json({ id: data.id })
+        } catch (error) {
+            return res.status(400).json({ status: 'erro!' })
+        }
     }
 }
